2c3abd6ea69b9101c67833b5cda2be5e2fa3305c
mhmood88hz-cloud <mhmood88hz@gmail.com>
2026-05-18 16:05:29 +0200

    Feat: Expand ASCII stages to 7 phases for smoother melting transition

--- ascii_art.py
+++ ascii_art.py
@@ -1,24 +1,43 @@
+# Expanded Snowman ASCII Art stages for a smoother meltdown
 STAGES = [
-     """
+    # Stage 0: Full snowman
+    """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
      ( : ) 
      """,
-     """
-      ___  
-     /___\\ 
+    # Stage 1: Hat disappears
+    """
      (o o) 
      ( : ) 
+     ( : ) 
      """,
-     """
-      ___  
-     /___\\ 
-     (o o) 
+    # Stage 2: Eyes disappear
+    """
+     (. .) 
+     ( : ) 
+     ( : ) 
      """,
-     """
-      ___  
-     /___\\ 
+    # Stage 3: Head starts melting
+    """
+      ...  
+     ( : ) 
+     ( : ) 
+     """,
+    # Stage 4: Head completely gone
+    """
+     ( : ) 
+     ( : ) 
+     """,
+    # Stage 5: Middle part melts
+    """
+     ( : ) 
+     """,
+    # Stage 6: Completely melted puddle
+    """
+
+     _______
      """
 ]
